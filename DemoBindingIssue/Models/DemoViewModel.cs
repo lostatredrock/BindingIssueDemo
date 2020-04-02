de8b8cdddf3a88187f1d2ee8929afd170d56bdc6
Andrew Nagel <andrew.nagel.ctr@dot.gov>
2020-04-02 08:57:10 -0400

    Code Upload

---
+++ DemoBindingIssue/Models/DemoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoBindingIssue.Models
+{
+    public class DemoViewModel
+    {
+        public string Val1 { get; set; }
+        public string Val2 { get; set; }
+    }
+}
